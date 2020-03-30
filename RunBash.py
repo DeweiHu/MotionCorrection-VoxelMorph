51bfe1ec3950784713f05784fa693fc77467d29f
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2020-03-30 09:33:59 -0400

    Add files via upload

--- RunBash.py
+++ RunBash.py
@@ -6,6 +6,11 @@ Created on Wed Mar 25 10:56:01 2020
 @author: dewei
 """
 
+import sys
+sys.path.insert(0,'/home/dewei/Desktop/Denoise/')
+import MyFunctions
+
+import os
 import subprocess
 import pickle
 import numpy as np
@@ -17,33 +22,40 @@ root = '/home/dewei/Desktop/Registered_volume/'
 temp = '/home/dewei/Desktop/slc/'
 radius = 7
 
-with open(root+'pack_warp_a.pickle','rb') as f:
-    volume = pickle.load(f)
+volumelist = []
+for file in os.listdir(root):
+    if file.endswith('.pickle'):
+        volumelist.append(file)
 
-volume_sf = np.zeros([len(volume),1024,512],dtype=np.float32)
+
+#%%
 
 t1 = time.time()
 
-for i in range(len(volume)):
-    # Go through all sclices, do self-fusion
-    pack = np.squeeze(volume[i],axis=1)
-    for j in range(2*radius+1):
-        mov = Image.fromarray(pack[j,:,:])
-        mov.save(temp+'atlas{}.tif'.format(j))
-    fix = Image.fromarray(pack[-1,:,:])
-    fix.save(temp+'fix_img.tif')
-    subprocess.call("/home/dewei/self_fusion.sh")
+for vol in range(len(volumelist)):
+    filename = volumelist[vol]
+    with open(root+filename,'rb') as f:
+        volume = pickle.load(f)
     
-    # load the self-fusion result and save in a volume
-    volume_sf[i,:,:] = io.imread(temp+'synthResult.tif')
+    volume_sf = np.zeros([len(volume),1024,512],dtype=np.float32)
+    
+    for i in range(len(volume)):
+        # Go through all sclices, do self-fusion
+        pack = np.squeeze(volume[i],axis=1)
+        for j in range(2*radius+1):
+            mov = Image.fromarray(pack[j,:,:])
+            mov.save(temp+'atlas{}.tif'.format(j))
+        fix = Image.fromarray(pack[-1,:,:])
+        fix.save(temp+'fix_img.tif')
+        subprocess.call("/home/dewei/self_fusion.sh")
+        
+        # load the self-fusion result and save in a volume
+        volume_sf[i,:,:] = io.imread(temp+'synthResult.tif')
+    
+    MyFunctions.nii_saver(volume_sf,root,filename[:-7]+'SF.nii.gz')
+    
+    del volume_sf,volume
     
 t2 = time.time()
 print('time: {} min'.format((t2-t1)/60))
-
-#%%
-import sys
-sys.path.insert(0,'/home/dewei/Desktop/Denoise/')
-import MyFunctions
-
-MyFunctions.nii_saver(volume_sf,root,'test_a.nii.gz')
 
